88a66af6df9280edcf8f419067f5d81e86d2ad3d
Pavithra <pavithra-gomathinay@pluralsight.com>
2024-05-12 23:54:24 +0530

    added card

--- scripts/details.js
+++ scripts/details.js
@@ -18,31 +18,31 @@ function getCourse(cid) {
     .then(course => {
     // this returns a single course!
     const container = 
-        document.getElementById('courseContainerDiv');
+        document.getElementById('courseList');
 
     // display one course property in a <p>
-    const deptP = document.createElement('p');
+    const deptP = document.createElement('li');
     deptP.textContent = `Name: ${course.dept}`;
     container.appendChild(deptP);
 
     // repeat for each field you want to display
-    const deptP1 = document.createElement('p');
+    const deptP1 = document.createElement('li');
     deptP1.textContent = `CourseNum: ${course.courseNum}`;
     container.appendChild(deptP1);
 
-    const deptP2 = document.createElement('p');
+    const deptP2 = document.createElement('li');
     deptP2.textContent = `CourseName: ${course.courseName}`;
     container.appendChild(deptP2);
 
-    const deptP3 = document.createElement('p');
+    const deptP3 = document.createElement('li');
     deptP3.textContent = `Instructor: ${course.instructor}`;
     container.appendChild(deptP3);
 
-    const deptP4 = document.createElement('p');
+    const deptP4 = document.createElement('li');
     deptP4.textContent = `StartDate: ${course.startDate}`;
     container.appendChild(deptP4);
 
-    const deptP5 = document.createElement('p');
+    const deptP5 = document.createElement('li');
     deptP5.textContent = `NumDays: ${course.numDays}`;
     container.appendChild(deptP5);
 
